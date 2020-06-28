f8839aadddf6c1363bb4b2bbabff28186736e70c
Ko3ch <tomko3ch@gmail.com>
2020-06-28 15:14:58 +0300

    added  docstrings

--- credentials-test.py
+++ credentials-test.py
@@ -6,17 +6,29 @@ class TestCredentials(unittest.TestCase):
     provide test cases to test Credentials class
     '''
     def setUp(self):
+        '''
+        create instance of Credentials
+        '''
         self.new_account = Credentials("twitter","9000")
         self.new_account.save_acc_details()
 
     def test_instance(self):
+        '''
+        test case to check if the class instanciates
+        '''
         self.assertEqual(self.new_account.account_name, "twitter")
         self.assertEqual(self.new_account.account_password, "9000")
 
     def test_save_acc_details(self):
+        '''
+        test case to check if a users accounts can added to acc_details
+        '''
         self.new_account.save_acc_details()
 
     def test_delete_account(self):
+        '''
+        test case to check if we can delete an account
+        '''
         self.new_account.save_acc_details()
         self.test_account = Credentials("Test","User")
         self.test_account.save_acc_details()
@@ -25,6 +37,9 @@ class TestCredentials(unittest.TestCase):
         self.assertEqual(len(Credentials.acc_details),1)
 
     def test_find_account_by_name(self):
+        '''
+        test to check if an account can be found by the account name
+        '''
         self.test_user = Credentials("Test","0000")
         self.test_user.save_acc_details()
 
--- user-test.py
+++ user-test.py
@@ -14,17 +14,29 @@ class TestUser(unittest.TestCase):
         self.new_user.add_user()
 
     def test_instance(self): 
+        '''
+        test case to check whether User is instanciating
+        '''
         self.assertEqual(self.new_user.user_name, "tom")
         self.assertEqual(self.new_user.password, "890000")
         self.assertEqual(self.new_user.credentials, Credentials.acc_details)
 
     def test_add_user(self):
+        '''
+        test case for add_user()
+        '''
         self.new_user.add_user()
 
     def test_save_user_credentials(self):
+        '''
+        test case to check if credentials are being added successfully
+        '''
         self.assertEqual(self.new_user.credentials, Credentials.acc_details)
 
     def test_user_exists(self):
+        '''
+        test case to check if a user exists 
+        '''
         self.test_user = User("Test","0000",'')
         self.test_user.add_user()
         user_exists = User.user_exists("Test")
@@ -32,10 +44,16 @@ class TestUser(unittest.TestCase):
         self.assertTrue(user_exists)
 
     def test_diplay_users(self):
+        '''
+        test case to check if we can view all users 
+        '''
         self.new_user.add_user()
         self.assertEqual(User.display_users(), User.users)
 
     def test_find_user_by_name(self):
+        '''
+        test case to check if we can find a user by their user_name
+        '''
         self.test_user = User("Test","0000",'')
         self.test_user.add_user()
 
